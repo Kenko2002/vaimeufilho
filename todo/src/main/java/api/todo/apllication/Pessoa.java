0e8b91900150842b69632a3a00e2113321cbed9f
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2023-10-02 23:01:35 -0300

    Add files via upload

---
+++ todo/src/main/java/api/todo/apllication/Pessoa.java
@@ -0,0 +1,28 @@
+package api.todo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+    private String nome;
+	
+	@Column
+    private int idade;
+	
+	
+}
+
+
