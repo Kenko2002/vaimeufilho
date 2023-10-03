25a953953f2246d494b18b942dc6fb4dbbc318f4
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2023-10-02 23:02:05 -0300

    Add files via upload

---
+++ todo/src/main/java/api/todo/repository/GenericRepository.java
@@ -0,0 +1,13 @@
+package api.todo.repository;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import api.todo.model.Pessoa;
+
+@Primary
+@Repository
+public interface GenericRepository extends JpaRepository<Pessoa, Long> {
+    // Métodos personalizados do repositório, se necessário
+}
