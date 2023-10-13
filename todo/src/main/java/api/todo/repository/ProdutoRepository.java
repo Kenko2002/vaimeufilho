b9442b3d0c4b41f4145305b45336de17b3a681a4
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2023-10-13 16:36:39 -0300

    Delete todo directory

--- todo/src/main/java/api/todo/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package api.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import api.todo.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto,Long> {
-
-}
